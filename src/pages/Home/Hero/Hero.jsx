1bac2ff1d44d835145b09214f9d0616269fba111
soubj <sobujahmed124@gmail.com>
2023-09-20 21:59:32 +0600

    finished hero section styling

--- src/pages/Home/Hero/Hero.jsx
+++ src/pages/Home/Hero/Hero.jsx
@@ -5,12 +5,25 @@ import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import { useEffect, useState } from "react";
 
 const Hero = () => {
+  const [heroData, setHeroData] = useState([]);
+  useEffect(() => {
+    fetch("hero.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setHeroData(data);
+      });
+  }, []);
+
   return (
     <Swiper
+      slidesPerView={1}
       spaceBetween={30}
       centeredSlides={true}
+      loop={true}
       autoplay={{
         delay: 2500,
         disableOnInteraction: false,
@@ -20,16 +33,28 @@ const Hero = () => {
       }}
       navigation={true}
       modules={[Autoplay, Pagination, Navigation]}
-      className="mySwiper h-full w-full">
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 5</SwiperSlide>
-      <SwiperSlide>Slide 6</SwiperSlide>
-      <SwiperSlide>Slide 7</SwiperSlide>
-      <SwiperSlide>Slide 8</SwiperSlide>
-      <SwiperSlide>Slide 9</SwiperSlide>
+      className="h-screen w-full">
+      {heroData.map((data) => (
+        <SwiperSlide key={data.id}>
+          <div
+            className="bg-cover w-full h-full"
+            style={{ backgroundImage: `url(${data?.img})` }}>
+            <div className=" bg-gradient-to-r from-black/70 to-black/10 w-full h-full">
+              <div className="flex flex-col justify-center items-center w-8/12 mx-auto h-full  gap-10">
+                <h2 className="text-6xl text-white font-amaranth font-bold text-center">
+                  {data.headline}
+                </h2>
+                <h4 className="text-4xl font-bellefair text-white font-semibold text-center">
+                  {data.subheadline}
+                </h4>
+                <button className="bg-orange-500 px-5 py-3 rounded-lg text-white font-amaranth text-lg">
+                  {data.cta}
+                </button>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
     </Swiper>
   );
 };
