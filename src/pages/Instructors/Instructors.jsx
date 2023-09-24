510ef69db28301a885984c16c4d2fcffd9d65dbe
soubj <sobujahmed124@gmail.com>
2023-09-24 20:44:54 +0600

    finished insturctors page

--- src/pages/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors.jsx
@@ -1,13 +1,34 @@
+import { useEffect, useState } from "react";
 import MiniHero from "../../Shared/MiniHero/MiniHero";
-import instructor from "../../assets/summerCamp/instructor.avif";
+import instructorImg from "../../assets/summerCamp/instructor.avif";
+import Instructor from "./Instructor";
 
 const Instructors = () => {
+  const [instructors, setInstructors] = useState([]);
+
+  useEffect(() => {
+    fetch("instructors.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setInstructors(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div>
+    <div className="bg-teal-200/30 dark:bg-gray-950">
       <MiniHero
-        image={instructor}
+        image={instructorImg}
         title="Our Instructors"
         subTitle="Art & Craft / Instructors"></MiniHero>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-20 px-5 lg:px-0 max-w-7xl mx-auto">
+        {instructors.map((instructor) => (
+          <Instructor key={instructor.id} instructor={instructor}></Instructor>
+        ))}
+      </div>
     </div>
   );
 };
