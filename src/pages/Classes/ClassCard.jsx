382d703de7d2aac4d318a48ff7623a7352f62500
soubj <sobujahmed124@gmail.com>
2023-09-24 14:59:21 +0600

    classes modal done

--- src/pages/Classes/ClassCard.jsx
+++ src/pages/Classes/ClassCard.jsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
+import ClassesModal from "./ClassesModal";
+
 const ClassCard = ({ Class }) => {
-  const { img, name, age_group, schedule, price } = Class;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const { img, name, age_group, schedule, price, available_seats } = Class;
 
   return (
     <div className="border-2 border-amber-400 dark:border-emerald-300 p-5 rounded-lg relative shadow-xl shadow-yellow-400 dark:shadow-sky-400 mt-10">
@@ -17,17 +22,41 @@ const ClassCard = ({ Class }) => {
       <h6 className="text-center dark:text-white/95 font-amaranth text-lg">
         {age_group}
       </h6>
+      <p className="dark:text-white/95 text-center font-amaranth mt-2">
+        Available seats : {available_seats}
+      </p>
       <p className="text-center dark:text-white/95 font-amaranth text-base my-2">
         {schedule}
       </p>
       <div className="flex flex-col sm:flex-row mt-9 mb-5 items-center justify-center gap-8">
-        <button className="bg-rose-500 font-semibold w-28 p-3 rounded-lg text-white font-amita">
-          Show More
-        </button>
+        {isModalOpen ? (
+          <button
+            onClick={() => setIsModalOpen(!isModalOpen)}
+            className="bg-rose-500 font-semibold w-28 p-3 rounded-lg text-white font-amita">
+            Show Less
+          </button>
+        ) : (
+          <button
+            onClick={() => setIsModalOpen(!isModalOpen)}
+            className="bg-rose-500 font-semibold w-28 p-3 rounded-lg text-white font-amita">
+            Show More
+          </button>
+        )}
+
         <button className="bg-emerald-500 w-28 p-3 font-semibold rounded-lg text-white font-amita">
           Enroll
         </button>
       </div>
+
+      {/* this is the modal */}
+      {isModalOpen ? (
+        <ClassesModal
+          Class={Class}
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}></ClassesModal>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
