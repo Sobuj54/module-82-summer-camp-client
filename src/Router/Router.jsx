842be6cea6957b2a13bc2dc78ed47290f82b300a
soubj <sobujahmed124@gmail.com>
2023-09-25 20:24:11 +0600

    added lazy load

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,8 +3,12 @@ import App from "../App";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
-import Classes from "../pages/Classes/Classes";
-import Instructors from "../pages/Instructors/Instructors";
+// import Classes from "../pages/Classes/Classes";
+import { Suspense, lazy } from "react";
+// import Instructors from "../pages/Instructors/Instructors";
+
+const Classes = lazy(() => import("../pages/Classes/Classes"));
+const Instructors = lazy(() => import("../pages/Instructors/Instructors"));
 
 const Router = createBrowserRouter([
   {
@@ -14,11 +18,41 @@ const Router = createBrowserRouter([
       { path: "/", element: <Home></Home> },
       {
         path: "classes",
-        element: <Classes></Classes>,
+        element: (
+          <Suspense
+            fallback={
+              <div className="w-full flex items-center justify-center my-10">
+                <div
+                  className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                  role="status">
+                  <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                    Loading...
+                  </span>
+                </div>
+              </div>
+            }>
+            <Classes></Classes>
+          </Suspense>
+        ),
       },
       {
         path: "instructors",
-        element: <Instructors></Instructors>,
+        element: (
+          <Suspense
+            fallback={
+              <div className="w-full flex items-center justify-center my-10">
+                <div
+                  className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                  role="status">
+                  <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                    Loading...
+                  </span>
+                </div>
+              </div>
+            }>
+            <Instructors></Instructors>
+          </Suspense>
+        ),
       },
       {
         path: "login",
