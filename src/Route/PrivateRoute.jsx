86c37646ae6ad8726a1f4e68fb191ff5cbad6aec
soubj <sobujahmed124@gmail.com>
2023-12-19 20:47:40 +0600

    removed code duplicacy for spinner

--- src/Route/PrivateRoute.jsx
+++ src/Route/PrivateRoute.jsx
@@ -1,23 +1,14 @@
 import React from "react";
 import useContextApi from "../Hooks/useContextApi";
 import { Navigate, useLocation } from "react-router-dom";
+import Spinner from "../components/Spinner/Spinner";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContextApi();
   const location = useLocation();
 
   if (loading) {
-    return (
-      <div className="flex items-center justify-center h-screen w-full">
-        <div
-          className="inline-block text-cyan-500 h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
-          role="status">
-          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-            Loading...
-          </span>
-        </div>
-      </div>
-    );
+    return <Spinner></Spinner>;
   }
 
   if (user) {
